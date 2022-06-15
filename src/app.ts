8298cc3f5f5c277c29a3410822844258580a5c81
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-15 10:24:56 +0200

    Handle error messages for Order API calls

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import 'express-async-errors'
+import OrderRouter from './routes/OrderRouter'
 
 const app = express()
+
+app.use('/api/orders', OrderRouter)
 
 export default app
--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -2,29 +2,24 @@ import OrderModel from '../models/OrderModel'
 import { Handler } from 'express'
 
 export const getAll: Handler = async (req, res) => {
-  try {
-    const Orders = await OrderModel.find()
-    res.send(Orders)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Orders = await OrderModel.find()
+  res.send(Orders)
 }
 
 export const getOne: Handler = async (req, res) => {
-  try {
-    const Order = await OrderModel.findOne({ _id: req.params.id })
-    res.send(Order)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Order = await OrderModel.findOne({ _id: req.params.id })
+  if (Order) res.send(Order)
+  else res.status(404).send('Order Not Found')
 }
 
 export const modify: Handler = async (req, res) => {
   try {
-    const Order = await OrderModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    res.send(Order)
+    const Order = await OrderModel.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true })
+    if (Order) res.send(Order)
+    else res.status(404).send('Order Not Found')
   } catch (err) {
-    res.status(400).json(err)
+    if (err instanceof Error && err.message) res.status(400).send(err.message)
+    else throw err
   }
 }
 
